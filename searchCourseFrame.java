aad2e9c2a8492558cf0153aa97eb748bc41e3e64
watinya <angel900124@gmail.com>
2020-12-18 12:42:58 +0800

    upgrade searchCourse
    缺 查看選修學生清單

--- searchCourseFrame.java
+++ searchCourseFrame.java
@@ -1,6 +1,11 @@
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+
+import Member.ListBtnEditor;
+import Member.ListBtnRender;
+import Member.Member;
 
 import java.awt.event.*;
 import java.io.File;
@@ -9,10 +14,10 @@ import java.util.Arrays;
 
 public class searchCourseFrame extends JFrame implements ActionListener {
 	private JLabel Jlb_semester = new JLabel("學期：");
-	private JComboBox jcb_time;
+	JComboBox jcb_time;
+	private JTable jt;
 	private DefaultTableModel tableM;
 	
-    
     public searchCourseFrame()
     {
         super("高燕大課程平台 開課查詢");
@@ -20,7 +25,7 @@ public class searchCourseFrame extends JFrame implements ActionListener {
         c.setLayout(null);
                 
         //設定Jlb_semester大小位置及顯示字型
-        Jlb_semester.setLocation(468,32);
+        Jlb_semester.setLocation(469,13);
         Jlb_semester.setSize(94,46);
         Jlb_semester.setFont(new Font("微軟正黑體", Font.BOLD, 30));
         c.add(Jlb_semester);
@@ -34,12 +39,43 @@ public class searchCourseFrame extends JFrame implements ActionListener {
     	  }
     	});
     	jcb_time = new JComboBox(directories);
-        jcb_time.setLocation(559, 41);
+        jcb_time.setLocation(560, 22);
         jcb_time.setSize(130, 36);
         jcb_time.setFont(new Font("微軟正黑體",Font.BOLD,22));
         jcb_time.addActionListener(this);
         c.add(jcb_time);
         
+        //表格內容
+    	String[] columns = { "開課代號", "課程名稱", "學分數", "科目型態", "授課教師", "選修學生清單"};
+		tableM = new DefaultTableModel(null, columns) {
+	    	@Override
+	    	public boolean isCellEditable(int row, int column) {
+	    		if (column < 6)
+					return false;
+				else
+					return true;
+			}// 表格不允許被編輯
+	    };
+	    JTable courseTable = new JTable(tableM) ;
+	    courseTable.setFont(new Font("微軟正黑體", Font.PLAIN, 24));
+	    
+	    courseTable.getColumnModel().getColumn(5).setCellRenderer(new ListBtnRender());
+		courseTable.getColumnModel().getColumn(5).setCellEditor(new ListBtnEditor());
+
+		JScrollPane coursePane = new JScrollPane(courseTable);
+		TableColumn column;
+		for (int i = 0; i < columns.length; i++) {
+			column = courseTable.getColumnModel().getColumn(i);
+			if (i == 1) {
+				column.setPreferredWidth(300); //second column is bigger
+			} else {
+				column.setPreferredWidth(20);
+			}
+		}
+		coursePane.setLocation(14, 72);
+		coursePane.setSize(1166, 680);
+		c.add(coursePane);
+		
         //設定視窗
         setSize(1200, 800);
         setLocationRelativeTo(null);//視窗置中
@@ -52,7 +88,9 @@ public class searchCourseFrame extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == jcb_time) {
-			
+			String selectedSemester = (String) jcb_time.getSelectedItem();
+			Member.outputCourseList(selectedSemester, tableM);
         }
     }
+    
 }
--- teacherFunctionFrame.java
+++ teacherFunctionFrame.java
@@ -1,39 +1,74 @@
-package Member;
-
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.table.*;
 
-public class CourseList {
-	JTable jt;
+import Member.Teacher;
 
-	public CourseList(String[][] fileContent) {
-		JFrame frame = new JFrame("課表");
-		Container c = frame.getContentPane();
-		
-		//建立表格
-		String[] columns = { "開課代號", "課程名稱", "學分數", "科目型態", "授課教師"};
-		jt = new JTable(fileContent, columns);
-		jt.setPreferredScrollableViewportSize(new Dimension(400,300));
-	    TableColumn column=jt.getColumnModel().getColumn(0);
-	    //表格寬度
-	    column.setPreferredWidth(80);
-	    column=jt.getColumnModel().getColumn(1);
-	    column.setPreferredWidth(200);
-	    column=jt.getColumnModel().getColumn(2);
-	    column.setPreferredWidth(65);
-	    column=jt.getColumnModel().getColumn(3);
-	    column.setPreferredWidth(80);
-	    column=jt.getColumnModel().getColumn(4);
-	    column.setPreferredWidth(80);
-		c.add(new JScrollPane(jt), BorderLayout.CENTER);
+import java.awt.event.*;
 
+public class teacherFunctionFrame extends JFrame implements ActionListener {
+	private JButton Jbtn_Course = new JButton("開課查詢");
+    private JButton Jbtn_Score = new JButton("輸入成績");
+    private JButton Jbtn_generateScore = new JButton("產生授課課程成績");
+    private JButton Jbtn_ChangePW = new JButton("修改密碼");
+    private Teacher user;
+    
+    public teacherFunctionFrame(Teacher user)
+    {
+        super("高燕大課程平台 教授功能");
+        this.user = user;
+        Container c = getContentPane();
+        c.setLayout(null);
+                 
+        //設定開課查詢(清單、內容、學生清單)按鈕大小位置及顯示字型
+        Jbtn_Course.setLocation(63,148);
+        Jbtn_Course.setSize(218,127);
+        Jbtn_Course.setFont(new Font("微軟正黑體",Font.BOLD,22));
+        Jbtn_Course.addActionListener(this);
+        c.add(Jbtn_Course);
+       
+        //設定成績管理按鈕大小位置及顯示字型
+        Jbtn_Score.setLocation(627,148);
+        Jbtn_Score.setSize(218,127);
+        Jbtn_Score.setFont(new Font("微軟正黑體",Font.BOLD,22));
+        Jbtn_Score.addActionListener(this);
+        c.add(Jbtn_Score);
+        
+        //設定產生授課課程成績按鈕大小位置及顯示字型
+        Jbtn_generateScore.setLocation(345,148);
+        Jbtn_generateScore.setSize(218,127);
+        Jbtn_generateScore.setFont(new Font("微軟正黑體",Font.BOLD,22));
+        Jbtn_generateScore.addActionListener(this);
+        c.add(Jbtn_generateScore);
+        
+        //設定修改密碼按鈕大小位置及顯示字型
+        Jbtn_ChangePW.setLocation(910,148);
+        Jbtn_ChangePW.setSize(218,127);
+        Jbtn_ChangePW.setFont(new Font("微軟正黑體",Font.BOLD,22));
+        Jbtn_ChangePW.addActionListener(this);
+        c.add(Jbtn_ChangePW);
+       
+        //設定視窗
+        setSize(1200, 800);
+        setLocationRelativeTo(null);//視窗置中
+        //setLocation(300,200);
+        setResizable(false);//視窗放大按鈕無效
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
-		frame.setSize(1000, 800);
-		frame.setLocationRelativeTo(null);// 視窗置中
-		frame.setResizable(false);// 視窗放大按鈕無效
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == Jbtn_Course) {
+        	new searchCourseFrame();
+        }
+        else if(e.getSource() == Jbtn_Score) {
+        	
+        }
+        else if(e.getSource() == Jbtn_generateScore) {
+        	
+        }
+        else if(e.getSource() == Jbtn_ChangePW) {
+			new changePasswordFrame(user);
+        }
+    }
 }
